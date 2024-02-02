2ffb9f640e3606a9941ec9f6f60624f0e528b5bc
Yama56800 <thomawmahiaw@gmail.com>
2024-02-02 14:17:11 +0100

    responsive v2

--- src/composants/About/About.jsx
+++ src/composants/About/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
             <h2> Présentation</h2>
             <img className="photo-profil" src={photo} alt="photo de profil" />
             <div className="description">
-                <p>Je suis Thomas Mahias, un développeur frontend débutant. Passionné par la création d'expériences 
+                <p>Je suis Thomas Mahias, un développeur front-end junior. Passionné par la création d'expériences 
                     web de qualité, j'utilise HTML, CSS et JavaScript pour donner vie à des designs innovants.
                      Mon approche se concentre sur la performance, l'accessibilité et l'adaptabilité aux évolutions 
                      de l'industrie. Je suis ouvert aux collaborations et prêt à contribuer à des projets excitants.
--- src/composants/Form/Form.jsx
+++ src/composants/Form/Form.jsx
@@ -1,74 +1,114 @@
 import React, { useState } from 'react';
-import axios from 'axios'; // Importez axios pour effectuer la requête HTTP POST
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import emailjs from '@emailjs/browser';
 
-const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
+
+const Contact = () => {
+  const schema = yup.object({
+    name: yup.string().max(50).required('Merci de rentrer votre nom et prénom'),
+    email: yup.string().email('Merci de renter une adresse mail valide').max(255).required('Merci de rentrer une adresse mail'),
+    phone: yup.number().typeError('Merci de rentrer un numéro de téléphone valide').required('Merci de rentrer un numéro de téléphone'),
+    message: yup.string().required('Merci de rentrer un message'),
+  }).required();
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema),
   });
 
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
+  const onSubmit = data => {
+    const templateId = 'template_5yb1elr';
+    const serviceId = 'service_5ekk2xg';
+    const userId = '1pE5dkAnWFR8sGczr'; // Remplacez ces valeurs par vos identifiants réels
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      // Envoyez les données du formulaire au backend
-      const response = await axios.post('/api/send-email', formData); // Remplacez '/api/send-email' par l'URL de votre endpoint backend
-      console.log('Server response:', response.data);
-
-      setIsSubmitted(true);
-    } catch (error) {
-      console.error('Error sending email:', error);
-    }
+    emailjs.send(serviceId, templateId, data, userId)
+      .then((response) => {
+        console.log('Message sent successfully', response.status, response.text);
+        setIsSubmitted(true); // Mettre à jour l'état pour indiquer l'envoi réussi
+        reset(); // Réinitialiser le formulaire
+      })
+      .catch((err) => console.error('Failed to send the message, error:', err));
   };
 
   return (
     <div className="form" id='contact'>
       <div className="contact-form">
-        <h2>DÉMARRONS UN PROJET ENSEMBLE, N'HÉSITEZ PAS À ME CONTACTER</h2>
-        {!isSubmitted ? (
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                placeholder="Nom"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Adresse e-mail"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <textarea
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                placeholder="Votre message"
-                required
-              />
-            </div>
-            <button type="submit">ME CONTACTER</button>
-          </form>
+      <h2>DÉMARRONS UN PROJET ENSEMBLE, N'HÉSITEZ PAS À ME CONTACTER</h2>
+      {!isSubmitted ? (
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-group">
+          <label htmlfor="name" className="label-contact">
+            
+          </label>
+          <input
+            className="input-contact"
+            type="text"
+            id="name"
+            nanme="name"
+            placeholder="Nom et Prenom"
+            {...register('name')}
+          />
+          {errors.name && <p id="c-yup">{errors.name.message}</p>}
+          <label htmlfor="email" className="label-contact">
+            
+          </label>
+          <input
+            className="input-contact"
+            type="email"
+            id="email"
+            nanme="email"
+            placeholder="exemple@gmail.com"
+            {...register('email')}
+          />
+          {errors.email && <p id="c-yup">{errors.email.message}</p>}
+          <label htmlfor="phone" className="label-contact">
+           
+          </label>
+          <input
+            className="input-contact"
+            type="text"
+            id="phone"
+            nanme="phone"
+            placeholder="+12345678"
+            {...register('phone')}
+          />
+          {errors.phone && <p id="c-yup">{errors.phone.message}</p>}
+          <label for="Sujet" className="label-contact">
+            
+          </label>
+          <select
+            className="select-contact"
+            name="Subject"
+            {...register('subject')}
+          >
+            
+            <option value="devis">Devis</option>
+            <option value="questions">Questions</option>
+            <option value="Autre">Autre</option>
+          </select>
+
+          <label htmlfor="message" className="label-contact">
+            {' '}
+            
+          </label>
+          <textarea
+            className="message-contact"
+            placeholder="Merci de renseigner vos questions ou commentaires"
+            id="message"
+            cols="20"
+            rows="10"
+            name="message"
+            {...register('message')}
+          ></textarea>
+          {errors.message && <p id="c-yup">{errors.message.message}</p>}
+         
+
+          <button type="submit">ME CONTACTER</button>
+        </div>
+        </form>
         ) : (
           <div className="form-submitted-message">
             Votre message a été envoyé. Merci de nous avoir contacté !
@@ -79,4 +119,4 @@ const ContactForm = () => {
   );
 };
 
-export default ContactForm;
+export default Contact;
