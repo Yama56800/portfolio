53a1779d12e659825c79073c282d2cdd669c215e
Yama56800 <thomawmahiaw@gmail.com>
2024-02-09 18:01:20 +0100

    deploy

--- src/composants/Competences/Competences.jsx
+++ src/composants/Competences/Competences.jsx
@@ -5,29 +5,42 @@ export default function Competences() {
       <ul className="tab-competences">
         <h2>Mes Compétences</h2>
         <li className="competences-item html">
-          <p>HTML</p>
-            93%
+          <p><i class="fa-brands fa-html5"></i>HTML</p>
+          97%
         </li>
         <li className="competences-item css">
-          <p>CSS</p>
-            93%
+          <p><i class="fa-brands fa-css3-alt"></i>CSS</p>
+          97%
+        </li>
+        <li className="competences-item responsive">
+          <p>Responsive</p>
+          97%
         </li>
         <li className="competences-item scss">
-          <p>SCSS</p>
-            83%
+          <p><i class="fa-brands fa-sass"></i>SCSS</p>
+          93%
+        </li>
+        <li className="competences-item green">
+          <p><i class="fa-solid fa-tree-deciduous"></i>Green code</p>
+          85%
         </li>
         <li className="competences-item javascript">
-          <p>JAVASCRIPT</p>
-            79%
+         <p><i class="fa-brands fa-js"></i>JAVASCRIPT</p>
+          83%
         </li>
         <li className="competences-item react">
-          <p>REACT</p>
-            67%
+          <p><i class="fa-brands fa-react"></i>REACT</p>
+          77%
         </li>
-        <li className="competences-item competences-item-last redux">
+        <li className="competences-item seo">
+          <p>Référencement SEO</p>
+          65%
+        </li>
+        <li className="competences-item redux">
           <p>REDUX</p>
-            35%
+          55%
         </li>
+        
       </ul>
     </nav>
   );
