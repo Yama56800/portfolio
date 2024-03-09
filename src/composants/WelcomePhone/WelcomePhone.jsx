cdbedc6568167fe234b30189a7e0ed2a2bbbcb75
Yama56800 <thomawmahiaw@gmail.com>
2024-03-09 19:08:28 +0100

    0903 commit maj referencement7

--- src/composants/WelcomePhone/WelcomePhone.jsx
+++ src/composants/WelcomePhone/WelcomePhone.jsx
@@ -5,6 +5,10 @@ export default function Competences() {
         <div className="welcomephone-content">
             <h2 className="welcomephone-title"> Thomas Mahias Développeur <br/>Front-End et Artiste Numérique </h2>
         </div>
+        <a class="container-arrow scroll-to" href="#cards">
+            <span>
+            <i class="fa fa-angle-down" aria-hidden="true"></i>  </span>
+        </a>
     </div>
   );
 }
