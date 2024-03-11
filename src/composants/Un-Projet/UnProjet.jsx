cc19c05a4d89ae67e76c12d06ff2b5b89eccf55f
Yama56800 <thomawmahiaw@gmail.com>
2024-03-11 16:50:22 +0100

    1103 commit maj referencement3

--- src/composants/Un-Projet/UnProjet.jsx
+++ src/composants/Un-Projet/UnProjet.jsx
@@ -2,44 +2,58 @@ import React from "react";
 export default function Competences() {
   return (
     <div className="Unprojet" id="Unprojet">
-        <h2>Un Projets?</h2>
-        <div className="Unprojet-content">
-            <div className="box box1">
-                <h3>Créez votre site internet sur mesure</h3>
-                    <p>
-                        Donnez vie à vos idées avec un site web conçu spécialement pour vous. Que vous soyez un professionnel à la recherche d'une vitrine pour votre activité ou
-                        un particulier souhaitant partager votre passion, nous offrons des solutions personnalisées qui s'adaptent à vos besoins. Expérience utilisateur optimale,
-                        design sur mesure, et fonctionnalités innovantes sont au cœur de notre démarche pour assurer votre satisfaction et celle de vos visiteurs.
-                    </p>
-            </div>
-            <div className="box box2">
-                <h3>Mettez à jour votre site Internet</h3>
-                    <p>
-                        Le monde du web évolue rapidement, et il est crucial que votre site reste à la pointe. Nous sommes spécialisés dans la mise à jour de sites internet,
-                        en les transformant pour adopter les dernières technologies web. Que ce soit pour améliorer la performance, ajouter de nouvelles fonctionnalités, 
-                        ou simplement rafraîchir le design, notre équipe s'assure que votre site reste compétitif dans un environnement numérique en constante évolution.
-                    </p>
-            </div>
-            <div className="box box3">
-                <h3>Optimisez votre présence en ligne avec le SEO</h3>
-                    <p>
-                        Être visible sur internet est essentiel pour attirer et retenir l'attention de votre audience cible. Nous vous aidons à optimiser votre référencement
-                        naturel pour améliorer votre positionnement dans les résultats de recherche. En analysant votre contenu, en optimisant vos mots-clés et en peaufinant 
-                        la structure de votre site, nous boostons votre visibilité pour attirer plus de visiteurs qualifiés.
-                    </p>
-            </div>
-            <div className="box box4">
-                <h3>Conseils, Performance et Débuggage</h3>
-                    <p>
-                        Votre site est lent ? Vous rencontrez des problèmes ou des bugs frustrants ? Notre équipe vous offre des conseils experts pour maximiser 
-                        la performance de votre site web. Nous analysons en profondeur pour identifier les problèmes, proposons des solutions concrètes, et débuggons votre 
-                        site pour garantir une expérience utilisateur sans faille. Avec notre soutien, assurez-vous que votre site fonctionne à son plein potentiel, rapide, 
-                        sécurisé, et sans erreur.
-                    </p>
-            </div>
-
+      <h2>Un Projet?</h2>
+      <div className="Unprojet-content">
+        <div className="box box1">
+          <h3>Créez votre site internet sur mesure</h3>
+          <p>
+            Donnez vie à vos idées avec un site web conçu spécialement pour
+            vous. Que vous soyez un professionnel à la recherche d'une vitrine
+            pour votre activité ou un particulier souhaitant partager votre
+            passion, nous offrons des solutions personnalisées qui s'adaptent à
+            vos besoins. Expérience utilisateur optimale, design sur mesure, et
+            fonctionnalités innovantes sont au cœur de notre démarche pour
+            assurer votre satisfaction et celle de vos visiteurs.
+          </p>
         </div>
- 
+        <div className="box box2">
+          <h3>Mettez à jour votre site Internet</h3>
+          <p>
+            Le monde du web évolue rapidement, et il est crucial que votre site
+            reste à la pointe. Nous sommes spécialisés dans la mise à jour de
+            sites internet, en les transformant pour adopter les dernières
+            technologies web. Que ce soit pour améliorer la performance, ajouter
+            de nouvelles fonctionnalités, ou simplement rafraîchir le design,
+            notre équipe s'assure que votre site reste compétitif dans un
+            environnement numérique en constante évolution.
+          </p>
+        </div>
+        <div className="box box3">
+          <h3>Optimisez votre présence en ligne avec le SEO</h3>
+          <p>
+            Être visible sur internet est essentiel pour attirer et retenir
+            l'attention de votre audience cible. Nous vous aidons à optimiser
+            votre référencement naturel pour améliorer votre positionnement dans
+            les résultats de recherche. En analysant votre contenu, en
+            optimisant vos mots-clés et en peaufinant la structure de votre
+            site, nous boostons votre visibilité pour attirer plus de visiteurs
+            qualifiés.
+          </p>
+        </div>
+        <div className="box box4">
+          <h3>Conseils, Performance et Débuggage</h3>
+          <p>
+            Votre site est lent ? Vous rencontrez des problèmes ou des bugs
+            frustrants ? Notre équipe vous offre des conseils experts pour
+            maximiser la performance de votre site web. Nous analysons en
+            profondeur pour identifier les problèmes, proposons des solutions
+            concrètes, et débuggons votre site pour garantir une expérience
+            utilisateur sans faille. Avec notre soutien, assurez-vous que votre
+            site fonctionne à son plein potentiel, rapide, sécurisé, et sans
+            erreur.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
